ff4d8a6c6b5318f3e6510b6a38ed57f9d40fefa5
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2011-07-22 21:28:48 -0700

    * Added RiakSessionStateProvider

---
+++ VisualStudio2010/Sample.SessionStateProvider/RiakSessionStateConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Sample.SessionStateProvider
+{
+    class RiakSessionStateConfiguration : ConfigurationSection
+    {
+        public static RiakSessionStateConfiguration LoadFromConfig(string sectionName)
+        {
+            return (RiakSessionStateConfiguration)ConfigurationManager.GetSection(sectionName);
+        }
+
+        [ConfigurationProperty("timeout_in_ms", IsDefaultCollection = true, IsRequired = true)]
+        public int TimeoutInMilliseconds
+        {
+            get { return (int) this["timeout_in_ms"]; }
+            set { this["timeout_in_ms"] = value;  }
+        }
+    }
+}
---
+++ VisualStudio2010/Sample.SessionStateProvider/UnityBootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using CorrugatedIron;
+using CorrugatedIron.Comms;
+using CorrugatedIron.Config;
+using Microsoft.Practices.Unity;
+
+namespace Sample.SessionStateProvider
+{
+    public static class UnityBootstrapper
+    {
+        public static IUnityContainer Bootstrap()
+        {
+            // pull the configuration straight out of the app.config file using the appropriate section name
+            var clusterConfig = RiakClusterConfiguration.LoadFromConfig("riakClusterConfiguration");
+
+            var container = new UnityContainer();
+            // register the configuration instance with the IoC container
+            container.RegisterInstance<IRiakClusterConfiguration>(clusterConfig);
+
+            // register the default node factory (single instance)
+            container.RegisterType<IRiakNodeFactory, RiakNodeFactory>(new ContainerControlledLifetimeManager());
+            // register the default connection factory (single instance)
+            container.RegisterType<IRiakConnectionFactory, RiakConnectionFactory>(new ContainerControlledLifetimeManager());
+            // register the default cluster (single instance)
+            container.RegisterType<IRiakCluster, RiakCluster>(new ContainerControlledLifetimeManager());
+
+            // register the client creator (multiple instance)
+            container.RegisterType<IRiakClient, RiakClient>();
+
+            return container;
+        }
+    }
+}
